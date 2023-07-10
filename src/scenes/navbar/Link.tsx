9d3c1ce165f7d701a9f44538fd476763de84bab9
chelahcynthia <cchelangat989@gmail.com>
2023-07-10 22:48:44 +0300

    Creates Link.tsx file on the navbar folder

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Link = (props: Props) => {
+  return (
+    <div>Link</div>
+  )
+}
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon  } from "@heroicons/react/24/solid";
-import Logo from " @/assets/Logo.png";
+import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 
 
@@ -20,7 +20,25 @@ const Navbar = (props: Props) => {
         <div className={`${flexBetween} mx-auto w-5/6`} >
           <div className={`${flexBetween} w-full gap-16`}>
             {/* left side of the navbaf */}
-            
+            <img alt="logo" src={Logo} />
+
+            {/* Right side  */}
+            <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                    <p>Home</p>
+                    <p>Benefits</p>
+                    <p>Our Classes</p>
+                    <p>Contact Us</p>
+                 </div>
+                 <div className={`${flexBetween} gap-8`}>
+                    <p>Sign In</p>
+                    <button>Become a member</button>
+
+                 </div>
+
+            </div>
+
+
             </div>  
         </div>
 
