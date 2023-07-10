c38bd5a71d16b2583d6e09d9f0bc556c1e60fdd5
chelahcynthia <cchelangat989@gmail.com>
2023-07-10 22:06:19 +0300

    Created a navbar and started styling it

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
+import { Bars3Icon, XMarkIcon  } from "@heroicons/react/24/solid";
+import Logo from " @/assets/Logo.png";
+import Link from "./Link";
+
 
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  return (
-    <div>Navbar</div>
-  )
+    const flexBetween =  "flex items-center justify-between";
+    const [isMenuToggled, setIsMenuToggle] = useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
+
+
+  return <nav>
+    <div className={`${flexBetween} fixed top-0 z-30 w-full`}>
+        <div className={`${flexBetween} mx-auto w-5/6`} >
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* left side of the navbaf */}
+            
+            </div>  
+        </div>
+
+    </div>
+    
+  </nav>
 }
 
 export default Navbar
