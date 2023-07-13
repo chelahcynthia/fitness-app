fb0508b79e3b1402e05b9ed053de4ae695a00638
chelahcynthia <cchelangat989@gmail.com>
2023-07-13 22:29:22 +0300

    Creates shared/types.ts file to be used on different files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+enum selectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus"
+
+}
+
 
 function App() {
-  return <div className="app bg-gray-20 ">
-    <Navbar />
-    </div>;
+  const [selectedPage, setSelectedPage] = useState<selectedPage>("home");
+
+  return (
+    <div className="app bg-gray-20 ">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
+
