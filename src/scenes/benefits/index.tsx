39f632e4a465e28d6cf3f76b1427d4e21281468c
Cynthia Chelah <110015043+chelahcynthia@users.noreply.github.com>
2023-08-01 09:08:26 +0000

    Creates a reusable component HText.tsx on shared folder

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,5 +1,6 @@
 import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 
 
@@ -11,8 +12,19 @@ const Benefits = ({ setselectedPage} : Props) => {
   return (
     <section
     id="benefits"
-    className="mx-auto min-h-full"
-    >
+    className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+      onViewportEnter={() => setselectedPage(SelectedPage.Benefits)}
+      >
+     {/* HEADER SECTION */}
+     <div>
+      <h1 className="basis-3/5 font-montserrat text-3xl font-bold"></h1>
+     </div>
+
+
+
+      </motion.div>
+    
       
 
     </section>
