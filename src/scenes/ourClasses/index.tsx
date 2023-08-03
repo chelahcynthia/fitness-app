2d9a9e6afae5ea5b3ce21da7aa7b4abf32ff3d92
chelahcynthia <cchelangat989@gmail.com>
2023-08-03 19:37:08 +0300

    Creates an ourclasses folder

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,13 @@
+import { SelectedPage } from "@/shared/types";
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const OurClasses = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default OurClasses;
