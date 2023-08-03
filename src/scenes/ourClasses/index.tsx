e3e47fdb569276b7f5c8846f6c9c5d567b3e3520
chelahcynthia <cchelangat989@gmail.com>
2023-08-03 19:48:56 +0300

    Creates an array to hold the interfaces

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,4 +1,4 @@
-import { SelectedPage } from "@/shared/types";
+import { SelectedPage, ClassType } from "@/shared/types";
 import image1 from "@/assets/image1.png";
 import image2 from "@/assets/image2.png";
 import image3 from "@/assets/image3.png";
@@ -6,7 +6,7 @@ import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
 
-const classes: Array<string> = [
+const classes: Array<ClassType> = [
   {
     name: "Weight Training Classes",
     description:
