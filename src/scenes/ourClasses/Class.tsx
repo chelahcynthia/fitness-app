0ea457d22edd6db7a17e7fca7840af8a5f2e5692
chelahcynthia <cchelangat989@gmail.com>
2023-08-04 15:35:59 +0300

    Done styling Class file to be imported in index file

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-
 type Props = {
-    name: string;
-    decsription: string;
-    image: string;
-}
+  name: string;
+  decsription: string;
+  image: string;
+};
 
-const Class = (props: Props) => {
+const Class = ({ name, decsription, image }: Props) => {
+  const overlayStyles = `p-5 absolute z-30 flex
+  h-[380px] w-[450px] flex-col items-center justify-center
+  whitespace-normal bg-primary-500 text-center text-white
+  opacity-0 transition duration-500 hover:opacity-90`;
+
   return (
-    <div>Class</div>
-  )
-}
+    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+      <div className="{overlayStyles}">
+        <p className="mt-5">{name}</p>
+        <p className="mt-5">{decsription}</p>
+      </div>
+      <img alt={`${image}`} src={image} />
+    </li>
+  );
+};
+export default Class;
