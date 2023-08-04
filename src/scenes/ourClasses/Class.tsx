fc02a7ab8f96b9e1852824e65826fdccfd643e20
chelahcynthia <cchelangat989@gmail.com>
2023-08-04 15:22:28 +0300

    Creates a class.tsx file for ourclasses styles

---
+++ src/scenes/ourClasses/Class.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Class = (props: Props) => {
+  return (
+    <div>Class</div>
+  )
+}
--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -5,6 +5,9 @@ import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+// import Class from "./Class";
 
 const classes: Array<ClassType> = [
   {
@@ -45,10 +48,49 @@ type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const OurClasses = (props: Props) => {
-  <section id="ourclasses" className="w-full bg-primary-100 py-40">
-
-  </section>
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </div>
+        </motion.div>
+        {/* <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div> */}
+      </motion.div>
+      
+    </section>
+  );
 };
 
 export default OurClasses;
