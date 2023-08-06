7f268645ded5b41404f9cda8814dfc43b1b63d44
chelahcynthia <cchelangat989@gmail.com>
2023-08-06 22:38:27 +0300

    Creates a function that handles form validation

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { SelectedPage } from "@/shared/types"
+import {motion} from "framer-motion";
+import HText from "@/shared/HText";
+import { useForm } from "react-hook-form"
+import CotactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 
-type Props = {}
 
-const index = (props: Props) => {
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const ContactUs = ({setSelectedPage}: Props) => {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+  px-5 py-3 placceholder-white`;
+  const { register, trigger, formState:{ errors} } = useForm();
+
+
+
+
   return (
     <div>index</div>
   )
 }
+export default ContactUs;
