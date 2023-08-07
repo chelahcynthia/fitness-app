aa19f68936bd3785d12bf8a251434651fe8cc2d9
chelahcynthia <cchelangat989@gmail.com>
2023-08-07 12:29:10 +0300

    I am done with the app

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,10 +1,32 @@
+import Logo from "@/assets/Logo.png";
 
-
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <div>index</div>
-  )
-}
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. 
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Cynthia Chelangat</p>
+          
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Hello World!</p>
+          <p>(+254)716759285</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
