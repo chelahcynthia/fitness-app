b1d1d7bbb222edd302300e4d2ce21ac4c115d23e
chelahcynthia <cchelangat989@gmail.com>
2023-08-07 11:29:56 +0300

    Done working on header section of the contactus page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -25,44 +25,41 @@ const ContactUs = ({ setSelectedPage }: Props) => {
     }
   };
 
-  return ( 
-  <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
-    <motion.div
-    onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
-    >
-      {/* HEADER */}
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div
-         className="md:w-3/5"
-         initial="hidden"
-         whileInView="visible"
-         viewport={{ once: true, amount: 0.5 }}
-         transition={{ duration: 0.5 }}
-         variants={{
-           hidden: { opacity: 0, x: -50 },
-           visible: { opacity: 1, x: 0 },
-         }}
-         >
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>
-          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
           </HText>
           <p className="my-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
+        </motion.div>
 
+        {/* FORM AND IMAGE SECTION */}
+        
       </motion.div>
-
-      
-
-
-    </motion.div>
-
-  </section>
+    </section>
   );
 };
 export default ContactUs;
